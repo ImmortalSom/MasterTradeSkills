3ad384f8ebafaf93a2ba32698099a839c61286e5
karasik <you@example.com>
2025-02-11 17:57:51 +0300

    test - Rough Gritted Paper

--- lib/LibCrafts-1.0/Professions/Jevelcrafting.lua
+++ lib/LibCrafts-1.0/Professions/Jevelcrafting.lua
@@ -7,18 +7,10 @@ local Quality = lib.constants.qualities
 local SpellSource = lib.constants.spell_sources
 local RecipeSource = lib.constants.recipe_sources
 
--- module:NewCraft(7421, "Runed Copper Rod", 1, {SpellSource.LearnedAutomatically})
---    :SetResult(6218)
---    :AddReagent(6217, 1) -- Copper Rod
---    :AddReagent(10938, 1) -- Lesser Magic Essence
---    :AddReagent(10940, 1) -- Strange Dust
---    :Save()
-
 if lib.env.is_turtle_wow then
-    -- module:NewCraft(22434, "Charged Scale of Onyxia", 300, {SpellSource.Quest})
-    --    :SetResult(17968)
-    --    :AddReagent(16203, 2) -- Greater Eternal Essence
-    --    :AddReagent(16204, 2) -- Illusion Dust
-    --    :AddReagent(17967, 1) -- Refined Scale of Onyxia
-    --    :Save()
+    module:NewCraft(nul, "Rough Gritted Paper", 1, {SpellSource.LearnedAutomatically})
+        :SetResult(55150)
+        :AddReagent(2589, 2) -- Linen Cloth
+        :AddReagent(2835, 1) -- Rough Stone
+        :Save()
 end
