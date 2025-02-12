088fca7573d6286e8931546fc883693eacacf946
karasik <you@example.com>
2025-02-12 16:59:55 +0300

    add recipe trainer to 100

--- lib/LibCrafts-1.0/Professions/Jewelcrafting.lua
+++ lib/LibCrafts-1.0/Professions/Jewelcrafting.lua
@@ -8,12 +8,12 @@ local SpellSource = lib.constants.spell_sources
 local RecipeSource = lib.constants.recipe_sources
 
 if lib.env.is_turtle_wow then
-    module:NewCraft(0, "Rough Gritted Paper", 5, {SpellSource.LearnedAutomatically})
+    module:NewCraft(0, "Rough Gritted Paper", 1, {SpellSource.LearnedAutomatically})
         :SetResult(55150)
         :AddReagent(2589, 1) -- Linen Cloth
         :AddReagent(2835, 1) -- Rough Stone
         :Save()
-    module:NewCraft(0, "Rough Copper Ring", 5, {SpellSource.LearnedAutomatically})
+    module:NewCraft(0, "Rough Copper Ring", 1, {SpellSource.LearnedAutomatically})
         :SetResult(55156)
         :AddReagent(2840, 2) -- Copper Bar
         :Save()
@@ -113,12 +113,75 @@ if lib.env.is_turtle_wow then
         :AddReagent(2592, 2) -- Wool Cloth
         :AddReagent(2836, 2) -- Coarse Stone
         :Save()
-    module:NewCraft(0, "Topaz Studded Ring", 65, {SpellSource.Trainer})
+    module:NewCraft(0, "Topaz Studded Ring", 70, {SpellSource.Trainer})
         :SetResult(55170)
         :AddReagent(55156, 1) -- Rough Copper Ring
         :AddReagent(81094, 2) -- Amber Topaz
         :AddReagent(2880, 1) -- Weak Flux
         :AddReagent(55150, 1) -- Rough Gritted Paper
+        :Save()
+    module:NewCraft(0, "Amberstone Pendant", 80, {SpellSource.Trainer})
+        :SetResult(55172)
+        :AddReagent(2840, 6) -- Copper Bar
+        :AddReagent(81094, 3) -- Amber Topaz
+        :AddReagent(55245, 1) -- Polishing Oil
+        :AddReagent(55150, 4) -- Rough Gritted Paper
+        :Save()
+    module:NewCraft(0, "Amberstone Pendant", 80, {SpellSource.Trainer})
+        :SetResult(55172)
+        :AddReagent(2840, 6) -- Copper Bar
+        :AddReagent(81094, 3) -- Amber Topaz
+        :AddReagent(55245, 1) -- Polishing Oil
+        :AddReagent(55150, 4) -- Rough Gritted Paper
+        :Save()
+    module:NewCraft(0, "Lavish Gemmed Necklace", 80, {SpellSource.Trainer})
+        :SetResult(55171)
+        :AddReagent(2840, 8) -- Copper Bar
+        :AddReagent(81094, 1) -- Amber Topaz
+        :AddReagent(818, 1) -- Tigerseye
+        :AddReagent(2880, 1) -- Weak Flux
+        :AddReagent(1210, 1) -- Shadowgem
+        :Save()
+    module:NewCraft(0, "Deepmist Choker", 85, {SpellSource.Trainer})
+        :SetResult(55173)
+        :AddReagent(2840, 8) -- Copper Bar
+        :AddReagent(5498, 1) -- Small Lustrous Pearl
+        :AddReagent(818, 1) -- Tigerseye
+        :AddReagent(2880, 1) -- Weak Flux
+        :AddReagent(55150, 2) -- Rough Gritted Paper
+        :Save()
+    module:NewCraft(0, "Rough Bronze Ring", 90, {SpellSource.Trainer})
+        :SetResult(55174)
+        :AddReagent(2841, 2) -- Bronze Bar
+        :Save()
+    module:NewCraft(0, "Shimmering Bronze Ring", 90, {SpellSource.Trainer})
+        :SetResult(41308)
+        :AddReagent(55174, 1) -- Rough Bronze Ring
+        :AddReagent(55246, 1) -- Shimmering Oil
+        :AddReagent(2880, 2) -- Weak Flux
+        :AddReagent(55151, 2) -- Coarse Gritted Paper
+        :Save()
+    module:NewCraft(0, "Amber Orb", 95, {SpellSource.Trainer})
+        :SetResult(41309)
+        :AddReagent(2841, 2) -- Bronze Bar
+        :AddReagent(81094, 3) -- Amber Topaz
+        :AddReagent(55150, 4) -- Rough Gritted Paper
+        :AddReagent(55245, 1) -- Polishing Oil
+        :Save()
+    module:NewCraft(0, "Earthrock Loop", 100, {SpellSource.Trainer})
+        :SetResult(55176)
+        :AddReagent(55174, 1) -- Rough Bronze Ring
+        :AddReagent(2449, 3) -- Earthroot
+        :AddReagent(2836, 6) -- Coarse Stone
+        :AddReagent(2880, 4) -- Weak Flux
+        :Save()
+    module:NewCraft(0, "Encrusted Bronze Staff", 100, {SpellSource.Trainer})
+        :SetResult(55175)
+        :AddReagent(2841, 8) -- Bronze Bar
+        :AddReagent(1210, 1) -- Shadowgem
+        :AddReagent(81094, 1) -- Amber Topaz
+        :AddReagent(55151, 4) -- Coarse Gritted Paper
+        :AddReagent(55245, 2) -- Polishing Oil
         :Save()
 
 
